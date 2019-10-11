4d305ed89fca7feb4b8ddb429c43bbc17859292d
Carl Yao Agbenyega <carl.agbenyega@gmail.com>
2019-10-11 00:18:13 -0600

    Card List Component with styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component.jsx';
 
 class App extends Component {
 
@@ -20,7 +21,9 @@ class App extends Component {
   render (){
     return (
       <div className="App">
-        { this.state.monsters.map(monster => (<h1 key={monster.id}> { monster.name } </h1>)) }
+        <CardList>
+          { this.state.monsters.map(monster => (<h1 key={monster.id}> { monster.name } </h1>)) }
+        </CardList>
       </div>
     );
   }
