8aa04eb8437963b90dc7ba00c782614c687b9336
Carl Yao Agbenyega <carl.agbenyega@gmail.com>
2019-10-17 00:12:28 -0600

    Updates

---
+++ precache-manifest.a855597aa3cbb977caa6428f3b91281e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a81b2368dc687f2d9a02aadba1bbe7d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e2a7961d031ed67b568a",
+    "url": "/monsters-rolodex/static/css/main.31ea2774.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "e2a7961d031ed67b568a",
+    "url": "/monsters-rolodex/static/js/main.6fa0f3d7.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
